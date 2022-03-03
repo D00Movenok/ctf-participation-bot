678f1cf5119c1b6657944526c032dff761dbc8e7
Vadim Saprykin <sprkweb@yandex.ru>
2022-03-03 20:28:07 +0300

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# ctf-participation-bot
-✅ ❎ CTF participation Telegram Bot
+# ✅ ❎ CTF participation Telegram Bot
+
+Automatization of the social organization routine of joining a CTF event.
+
+1. Monitors your teams on CTFTime
+2. When a team joins a new event, a poll is created in your Telegram chat: who is going to participate?
+3. Generates new Discord channels specifically for this event.
+
+## Installation
+
+1. Install requirements (also requires Python 3)
+
+    $ pip install -r requirements.txt
+
+2. Configure all the options in the `config.py` file
+3. Start
+
+    $ python app.py
--- config.py
+++ config.py
@@ -1,17 +1,25 @@
 config = {
+    # Telegram API token
     'tg_token': '',
+    # Telegram chat ID
     'tg_chat_id': 123,
+    # The lowest acceptable amount of people to participate in an event
     'min_will_play': 4,
 
+    # Discord API token
     'ds_token': '',
+    # Discord server ID
     'ds_srv_id': 123,
+    # The names of the text channels you want to generate
     'text_channels': [
         'web', 'rev', 'crypto', 'misc',
     ],
+    # The names of the voice channels you want to generate
     'voice_channels': [
         'web', 'rev', 'crypto', 'misc',
     ],
 
+    # IDs of your teams on CTFTime
     'ctf_teams': [
         1, 2, 3,
     ],
