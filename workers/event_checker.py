224978e8f9cb0f68ac9cdcae5c4ade3a7005f59e
baffoRti <saha14373@gmail.com>
2022-03-03 17:07:24 +0300

    Event checker edit

--- workers/event_checker.py
+++ workers/event_checker.py
@@ -1,18 +1,16 @@
 import threading
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from sqlalchemy import select, update
 
 from common.models import Event, Voter
-from common.database import engine
+from common.database import session
 from config import config
 from actions.discord_create import create_discord_channels
 import time
 
 
 class EventChecker:
-    conn = engine.connect()
-
     def start(self):
         threading.Thread(target=self.__monitor_cycle).start()
 
@@ -22,19 +20,22 @@ class EventChecker:
             time.sleep(60)
 
     def check(self):
-        query1 = select(Voter.poll_id)
-        res1 = self.conn.execute(query1)
-        unique_arr = set()
-        for row in res1:
-            unique_arr.add(row[0])
-        for poll_id in unique_arr:
-            query2 = select(Voter).where(Voter.poll_id == poll_id, Voter.will_play == True)
-            res2 = self.conn.execute(query2)
-            arr = []
-            for row in res2:
-                arr.append(row)
-            if len(arr) >= config['min_will_play']:
-                query3 = (update(Event).returning(Event).where(Event.poll_id == poll_id).values(done=True))
-                create_discord_channels(self.conn.execute(query3))
-        query4 = (update(Event).where(Event.start_time > datetime.now()).values(done=True))
-        self.conn.execute(query4)
+        with session.begin() as local_session:
+            query = select(Voter.poll_id)
+            result = local_session.execute(query)
+            unique_arr = set()
+            for row in result:
+                unique_arr.add(row[0])
+            for poll_id in unique_arr:
+                query = select(Voter).where(Voter.poll_id == poll_id, Voter.will_play == True)
+                result = local_session.execute(query)
+                arr = []
+                for row in result:
+                    arr.append(row)
+                if len(arr) >= config['min_will_play']:
+                    query = (update(Event).where(Event.poll_id == poll_id).values(done=True))
+                    local_session.execute(query)
+                    query = select(Event).where(Event.poll_id == poll_id)
+                    create_discord_channels(local_session.execute(query))
+            query = (update(Event).where(Event.start_time < (datetime.now() + timedelta(days=1))).values(done=True))
+            local_session.execute(query)
