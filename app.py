5ce73a94e32edac772c87092c97541004cb2d810
D00Movenok <egor.gennadjev@yandex.ru>
2022-03-10 17:31:01 +0300

    add time to logging

--- app.py
+++ app.py
@@ -8,11 +8,15 @@ from workers import CtftimeMonitor, EventChecker, TelegramMonitor
 
 def main():
     if config['DEBUG']:
-        logging.basicConfig(level=logging.DEBUG)
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(levelname)s[%(asctime)s] %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
         logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
         logging.getLogger("sqlalchemy.pool").setLevel(logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.INFO,
+                            format='%(levelname)s[%(asctime)s] %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S')
 
     Base.metadata.create_all(engine)
 
